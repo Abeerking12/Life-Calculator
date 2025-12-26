80aef418a3b50e262cf6cb7e3d21b3cc80288df5
Professor AbheeBhai <160325811+Abeerking12@users.noreply.github.com>
2025-12-26 14:27:47 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("ghosty-cache").then(c => c.addAll(["./"]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
